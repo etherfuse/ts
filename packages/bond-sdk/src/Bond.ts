f9079548f3a8ed111341272c795b718d779918ec
AJ Taylor <anthonjtaylor@gmail.com>
2024-01-25 19:37:43 -0700

    bond idl import works

--- packages/bond-sdk/src/Bond.ts
+++ packages/bond-sdk/src/Bond.ts
@@ -1,19 +1,23 @@
-import { AnchorProvider, Program, Provider, Wallet } from '@coral-xyz/anchor';
-import { Connection } from '@solana/web3.js';
+import { AnchorProvider, Program, Provider, Wallet, Idl } from '@coral-xyz/anchor';
+import { Connection, PublicKey } from '@solana/web3.js';
 import { isEven } from '@etherfuse/utils';
-// import { BOND_IDL } from '@etherfuse/bond-idl';
+import { BOND_IDL } from '@etherfuse/bond-idl';
 
 export class Bond {
   private readonly _connection: Connection;
   private readonly _provider: Provider;
-  //  private _bondProgram: Program;
+  private _bondProgram: Program;
 
   constructor(connection: Connection, wallet: Wallet) {
     this._connection = connection;
     this._provider = new AnchorProvider(connection, wallet, {
       commitment: connection.commitment,
     });
-    //    this._bondProgram = new Program(BOND_IDL as Idl, BOND_PROGRAM_ID, this._provider);
+    this._bondProgram = new Program(
+      BOND_IDL as Idl,
+      new PublicKey('EfuseVF62VgpYmXroXkNww8qKCQudeHAEzczSAC7Xsir'),
+      this._provider
+    );
   }
 }
 //The Bond class allows the user to create a Bond object
