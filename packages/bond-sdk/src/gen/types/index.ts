12ab97ad8ad4c04bd6d1d2ff48ef1de45822c077
AJ Taylor <anthonjtaylor@gmail.com>
2024-01-25 20:36:57 -0700

    remove admin types

--- packages/bond-sdk/src/gen/types/index.ts
+++ packages/bond-sdk/src/gen/types/index.ts
@@ -1,9 +1,5 @@
 import * as OracleType from './OracleType';
 
-export { AddAdminParams } from './AddAdminParams';
-export type { AddAdminParamsFields, AddAdminParamsJSON } from './AddAdminParams';
-export { AddKycUserParams } from './AddKycUserParams';
-export type { AddKycUserParamsFields, AddKycUserParamsJSON } from './AddKycUserParams';
 export { BurnPassParams } from './BurnPassParams';
 export type { BurnPassParamsFields, BurnPassParamsJSON } from './BurnPassParams';
 export { CollectCouponParams } from './CollectCouponParams';
@@ -14,48 +10,10 @@ export { CollectParValueForNFTParams } from './CollectParValueForNFTParams';
 export type { CollectParValueForNFTParamsFields, CollectParValueForNFTParamsJSON } from './CollectParValueForNFTParams';
 export { CollectParValueParams } from './CollectParValueParams';
 export type { CollectParValueParamsFields, CollectParValueParamsJSON } from './CollectParValueParams';
-export { DisableKycUserParams } from './DisableKycUserParams';
-export type { DisableKycUserParamsFields, DisableKycUserParamsJSON } from './DisableKycUserParams';
 export { ExchangeTokensForNFTParams } from './ExchangeTokensForNFTParams';
 export type { ExchangeTokensForNFTParamsFields, ExchangeTokensForNFTParamsJSON } from './ExchangeTokensForNFTParams';
-export { FreezeCollectionParams } from './FreezeCollectionParams';
-export type { FreezeCollectionParamsFields, FreezeCollectionParamsJSON } from './FreezeCollectionParams';
-export { FundInterestPaymentParams } from './FundInterestPaymentParams';
-export type { FundInterestPaymentParamsFields, FundInterestPaymentParamsJSON } from './FundInterestPaymentParams';
-export { FundParValuePaymentParams } from './FundParValuePaymentParams';
-export type { FundParValuePaymentParamsFields, FundParValuePaymentParamsJSON } from './FundParValuePaymentParams';
-export { InitCollectionParams } from './InitCollectionParams';
-export type { InitCollectionParamsFields, InitCollectionParamsJSON } from './InitCollectionParams';
-export { InitGlobalPassConfigParams } from './InitGlobalPassConfigParams';
-export type { InitGlobalPassConfigParamsFields, InitGlobalPassConfigParamsJSON } from './InitGlobalPassConfigParams';
 export { MintBondParams } from './MintBondParams';
 export type { MintBondParamsFields, MintBondParamsJSON } from './MintBondParams';
-export { RemoveAdminParams } from './RemoveAdminParams';
-export type { RemoveAdminParamsFields, RemoveAdminParamsJSON } from './RemoveAdminParams';
-export { SetCollectionInterestRateParams } from './SetCollectionInterestRateParams';
-export type {
-  SetCollectionInterestRateParamsFields,
-  SetCollectionInterestRateParamsJSON,
-} from './SetCollectionInterestRateParams';
-export { SetCollectionLimitParams } from './SetCollectionLimitParams';
-export type { SetCollectionLimitParamsFields, SetCollectionLimitParamsJSON } from './SetCollectionLimitParams';
-export { SetCollectionMaturityDateParams } from './SetCollectionMaturityDateParams';
-export type {
-  SetCollectionMaturityDateParamsFields,
-  SetCollectionMaturityDateParamsJSON,
-} from './SetCollectionMaturityDateParams';
-export { SetCollectionMintParams } from './SetCollectionMintParams';
-export type { SetCollectionMintParamsFields, SetCollectionMintParamsJSON } from './SetCollectionMintParams';
-export { SetCollectionNFTParams } from './SetCollectionNFTParams';
-export type { SetCollectionNFTParamsFields, SetCollectionNFTParamsJSON } from './SetCollectionNFTParams';
-export { SetFundingAccountsParams } from './SetFundingAccountsParams';
-export type { SetFundingAccountsParamsFields, SetFundingAccountsParamsJSON } from './SetFundingAccountsParams';
-export { SetGlobalPassConfigParams } from './SetGlobalPassConfigParams';
-export type { SetGlobalPassConfigParamsFields, SetGlobalPassConfigParamsJSON } from './SetGlobalPassConfigParams';
-export { SetNFTParams } from './SetNFTParams';
-export type { SetNFTParamsFields, SetNFTParamsJSON } from './SetNFTParams';
-export { UnfreezeCollectionParams } from './UnfreezeCollectionParams';
-export type { UnfreezeCollectionParamsFields, UnfreezeCollectionParamsJSON } from './UnfreezeCollectionParams';
 export { ViewCouponReturnsForNftParams } from './ViewCouponReturnsForNftParams';
 export type {
   ViewCouponReturnsForNftParamsFields,
